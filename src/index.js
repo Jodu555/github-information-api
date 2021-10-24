4d079281eaadf250daf86045f17d990103604204
Jodu555 <Jodu505@gmail.com>
2021-10-24 17:50:29 +0200

    added the whole github project page parsing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 app.use(cors());
@@ -14,8 +15,29 @@ app.get('/:username', async (req, res, next) => {
     const username = req.params.username;
     const response = await axios.get(`https://github.com/${username}?tab=repositories`);
     const html = response.data;
+    const $ = cheerio.load(html);
+    if (typeof $('#user-repositories-list').html() == 'string') {
+        const $repoList = cheerio.load($('#user-repositories-list').html());
+        const repositories = [];
+        $repoList('li').each((i, item) => {
+            const $repo = cheerio.load($repoList(item).html());
+            const name = $repo('[itemprop="name codeRepository"]').text().trim();
+            const description = $repo('[itemprop="description"]').text().trim();
+            const language = $repo('[itemprop="programmingLanguage"]').text().trim();
+            const repoHtml = $repo.html();
+            //!Here i dont know the way to parse this with cheerio so the good old split mehtod comes!
+            const datetime = repoHtml.split('<relative-time')[1].split(' ')[1].split('"')[1];
 
-    res.json({});
+            repositories.push({
+                name,
+                description,
+                language,
+                lastUpdated: datetime,
+            });
+        });
+        res.json({ repositories });
+    }
+
 });
 
 const PORT = process.env.PORT || 3100;
