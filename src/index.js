f68f507847d70e55ab8eb8a238ff0995c72f0ec5
Jodu555 <Jodu505@gmail.com>
2021-10-24 17:19:14 +0200

    setted up the basic html get with axios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -9,9 +10,15 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json());
 
+app.get('/:username', async (req, res, next) => {
+    const username = req.params.username;
+    const response = await axios.get(`https://github.com/${username}?tab=repositories`);
+    const html = response.data;
 
+    res.json({});
+});
 
 const PORT = process.env.PORT || 3100;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Express App is listening on ${PORT}`);
 });
