299e3d6b4fecf1aa492fbd49e184c21bd94ef6e8
Jodu555 <Jodu505@gmail.com>
2021-10-24 18:57:37 +0200

    commented + add a langauge devision file

--- src/route/index.js
+++ src/route/index.js
@@ -3,8 +3,9 @@ const controller = require('./controller.js');
 const router = express.Router();
 
 
-router.get('/all/:username', controller.getAll)
-router.get('/lastCommit/:username', controller.getLatestCommit)
+router.get('/all/:username', controller.getAll); //Returns all Public projects with title, description, lastUpdate and language
+router.get('/lastCommit/:username', controller.getLatestCommit); //Returns the latest updated project
+router.get('/languagaeDevision/:username', controller.getLatestCommit); // Returns the language devision
 
 
 module.exports = { router, }
