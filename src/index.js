81cd36031f509edfd36c9c9b0bdb629783714b2a
Jodu555 <Jodu505@gmail.com>
2021-10-25 11:29:01 +0200

    added the code to get the info on how many commits per day for every day in year

--- src/index.js
+++ src/index.js
@@ -18,7 +18,29 @@ const { errorHandling, notFound } = require('./middleware');
 app.use('*', notFound);
 app.use(errorHandling);
 
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const PORT = process.env.PORT || 3100;
 app.listen(PORT, async () => {
     console.log(`Express App is listening on ${PORT}`);
+
+    const response = await axios.get(`https://github.com/Jodu555`);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const dayInfo = {};
+
+    const $calendarGraph = cheerio.load($('svg.js-calendar-graph-svg').html());
+    $calendarGraph('g').each((i, graphGroups) => {
+        const $groupItem = cheerio.load($calendarGraph(graphGroups).html());
+        $groupItem('rect').each((j, rect) => {
+            const attributes = $groupItem(rect).attr();
+            dayInfo[attributes['data-date']] = {
+                count: attributes['data-count'],
+            }
+        });
+    });
+
+    console.log(dayInfo);
 });
